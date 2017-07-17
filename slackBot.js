9093095f99ac7dacd610b30555d5b5e7da43c9de
sukwhan (david) <younsa@bc.edu>
2017-07-17 15:42:10 -0700

    david

--- slackBot.js
+++ slackBot.js
@@ -1,14 +1,32 @@
+// express
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+var port = '3000'
+app.listen(port);
+
+// bot
+
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-// var SLACK_API_TOKEN="I8bPfR2zFGg6U6pjxXKkJo3I";
-// use env.sh
-var token = process.env.SLACK_API_TOKEN || '';
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS; //
 
-var rtm = new RtmClient(token, { logLevel: 'debug' });
+// use env.sh
+var token = 'xoxb-213345309616-Pcjs9E0KQts5QvTavryg4ByK';
+// var token = process.env.SLACK_API_TOKEN || '';
+
+var rtm = new RtmClient(token);
 rtm.start();
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   console.log('Message:', message);
+  rtm.sendMessage("this is response "+ message.text, message.channel)
 });
 
 rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
@@ -17,4 +35,9 @@ rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
 
 rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
   console.log('Reaction removed:', reaction);
+});
+
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+  // rtm.sendMessage("Hello!", channel);
+  console.log("Bot is online guys!");
 });
