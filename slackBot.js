1414c5c2aafe3e69c8620c4119109e9590d53dc2
sukwhan (david) <younsa@bc.edu>
2017-07-18 10:49:08 -0700

    david

--- slackBot.js
+++ slackBot.js
@@ -12,31 +12,42 @@ var axios = require('axios');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var pending = false;
+var pendingExist = false; //link to mongoDB with slackID and pending Varaible.
 
 app.post ('/messageReceive', function(req, res) {
   console.log("@@@@@@@@@@@@PAYLOAD @@@@ ", req);
   var payload = JSON.parse(req.body.payload);
-  // console.log(payload);
-  // while (payload.actions[0].value !== 'true' && payload.actions[0].value !== 'false' ) {
-  //   res.send('Please press cancel before entering new schedule.');
-  // }
+
   if (payload.actions[0].value === 'true'){
-    res.send('Created reminder! :+1:')
-    pending = false
+    res.send({
+      text: 'Created! :white_check_mark:',
+      // attachments: [
+      //   {
+      //       "color": "#36a64f",
+      //       // "pretext": "AWESOME! SO I'll create a reminder for you.",
+      //       "title": "AWESOME! SO I'll create a reminder for you.",
+      //       "text": "Optional text that appears within the attachment",
+      //       "fields": [
+      //           {
+      //               "title": "Subject",
+      //               "value": "Richard",
+      //               "short": true
+      //           },
+      //           {
+      //               "title": "Day",
+      //               "value": "High",
+      //               "short": true
+      //           }
+      //       ]
+      //   }
+      // ]
+    })
+    pendingExist = false
   } else if (payload.actions[0].value === 'false'){
     res.send('Canceled :x:');
-    pending = false
+    pendingExist = false
   }
-
-  // } else{
-  //   res.send('Please press cancel before entering new schedule.')
-  // }
 })
-
-// app.get('/messageReceive',function(req, res){
-//
-// })
 
 var port = '3000'
 app.listen(port);
@@ -54,6 +65,7 @@ var web = new WebClient(token);
 var rtm = new RtmClient(token);
 rtm.start();
 
+// when I receive message from SlackBot
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   var dm = rtm.dataStore.getDMByUserId(message.user);
 
@@ -61,45 +73,21 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   if (!dm || dm.id !== message.channel || message.type!== 'message'){
     console.log("Message not sent to DM, ignoring");
     console.log("dm" , dm);
-    console.log('message', message);
+    console.log('NOT Direct Message: ', message);
     return;
   }
   //if it is DM.
-  console.log('Message:', message);
+  console.log('Direct Message: ', message);
 
-  var jsonBtn = {
-    // "text": "Would you like to play a game?",
-    "attachments": [
-        {
-            "fallback": "You are unable to create a schedule",
-            "callback_id": "confirm_or_not",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "confirm",
-                    "text": "Confirm",
-                    "type": "button",
-                    "value": "true"
-                },
-                {
-                    "name": "cancel",
-                    "text": "Cancel",
-                    "type": "button",
-                    "value": "false"
-                }
-              ]
-          }
-      ]
-  }
-
-  // curl 'https://api.api.ai/api/query?v=20150910&query=remind%20me&lang=en&sessionId=13756478-6ee1-48f8-9953-7f53da5e2206&timezone=2017-07-17T16:55:51-0700' -H 'Authorization:Bearer e637efb67d9e44abbb260b09b472af21'
-
-  if (pending) {
-    rtm.sendMessage('I think you\`re trying to create a new reminder. If so, please press `cancel` first to abort the current reminder.', message.channel)
+  cancelBtn = {}
+  // if pending is true, alert user to finish the pending task.
+  if (pendingExist) {
+    rtm.sendMessage("I think you're trying to create a new reminder. If so, please press `cancel` first to about the current reminder", message.channel)
+    // web.chat.postMessage(message.channel, `Scheduling a meeting with ${data.result.parameters.invitees} on ${data.result.parameters.date} at ${data.result.parameters.time} `, jsonBtn)
     return;
   }
 
+  // curl 'https://api.api.ai/api/query?v=20150910&query=remind%20me&lang=en&sessionId=13756478-6ee1-48f8-9953-7f53da5e2206&timezone=2017-07-17T16:55:51-0700' -H 'Authorization:Bearer e637efb67d9e44abbb260b09b472af21'
   axios.get('https://api.api.ai/api/query', {
     params: {
       v: 20150910,
@@ -114,19 +102,69 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   })
   .then(function({data}) {
 
-    console.log("DATA" , data);
+    console.log("DATA: " , data);
+
+    // if some input is missing,
     if (data.result.actionIncomplete) {
       rtm.sendMessage(data.result.fulfillment.speech, message.channel);
     }
-    else {
-      //TODO When I have everything what I need. ex. date & todo.
+    else { //When I have everything what I need. ex. date & todo.
+      var jsonBtn = {
+        // "text": "Would you like to play a game?",
+        "attachments": [
+            {
+                // "title": "Is this reminder correct?",
+                "fallback": "You are unable to create a schedule",
+                "attachment_type": "default",
+                "fields": [
+                          {
+                              "title": "Subject",
+                              "value": data.result.parameters.subject,
+                              "short": true
+                          },
+                          {
+                              "title": "Date",
+                              "value": data.result.parameters.date,
+                              "short": true
+                          }
+                      ]
+              },
+              {
+                  // "title": "Is this reminder correct?",
+                  "fallback": "You are unable to create a schedule",
+                  "callback_id": "confirm_or_not",
+                  "color": "#3AA3E3",
+                  "attachment_type": "default",
+
+                  "title": "Is this reminder correct?",
+                  "actions": [
+                      {
+                          "name": "confirm",
+                          "text": "Yes",
+                          "type": "button",
+                          "value": "true"
+                      },
+                      {
+                          "name": "cancel",
+                          "text": "Cancel",
+                          "type": "button",
+                          "style": "danger",
+                          "value": "false"
+                      }
+                    ]
+                }
+          ]
+      }
       console.log('Action is complete!!!', data.result.parameters);
-      pending = true;
+      pendingExist = true; //set it to true for future use.
+
       // ACTION IS COMPLETE {date: '2017-07-26', description: 'do laundry', ...}
+
+      // if invitees exist
       if (data.result.parameters.invitees) {
-        web.chat.postMessage(message.channel, `:date: :thinking_face: Scheduling a meeting with ${data.result.parameters.invitees} on ${data.result.parameters.date} at ${data.result.parameters.time} `, jsonBtn)
+        web.chat.postMessage(message.channel, `Scheduling a meeting with ${data.result.parameters.invitees} on ${data.result.parameters.date} at ${data.result.parameters.time} `, jsonBtn)
       } else {
-        web.chat.postMessage(message.channel, `:stopwatch: :thinking_face: Creating reminder for ${data.result.parameters.subject} on ${data.result.parameters.date} `, jsonBtn)
+        web.chat.postMessage(message.channel,'', jsonBtn)
       }
     }
   })
@@ -139,8 +177,6 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   // }
 
   // rtm.sendMessage("this is response "+ message.text, message.channel)
-
-
 
   // web.chat.postMessage(message.channel, 'Hello there', jsonBtn, function(err, res) {
   //   if (err) {
@@ -161,5 +197,5 @@ rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) 
 
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
   // rtm.sendMessage("Hello!", channel);
-  console.log("Bot is online guys!");
+  console.log("Bot is online!");
 });
