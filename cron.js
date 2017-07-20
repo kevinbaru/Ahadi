834c4e87411139ac8a3a10e22e57d7175c5cf7d4
git push -u origin <baru.kevin3@gmail.com>
2017-07-19 22:17:37 -0700

    cron

--- cron.js
+++ cron.js
@@ -4,10 +4,23 @@ console.log('Hello I am runnin!!')
 
 var{User, Reminder}= require('./models');
 var {web}= require('./slackBot');
- User.findOne()
- .then(function(user){
-   web.chat.postMessage(user.slackDMId,'Current time is '+ new Date(),function() {
-     process.exit(0)
-   });
+var moment =moment()
+Reminder.find({date:{$gt: Date.now(), $lt:  new Date(Date.now() + (24 * 60 * 60 * 1000))}})
+.then(function(reminders){
+  reminders.map((reminder)=>{
+    web.chat.postMessage(reminder.user.slackDMId,'REMINDER: '+ reminder.subject,function() {
+      process.exit(0)
+    });
 
- })
+  })
+
+
+})
+
+ // User.findOne()
+ // .then(function(user){
+ //   web.chat.postMessage(user.slackDMId,'Current time is '+ new Date(),function() {
+ //     process.exit(0)
+ //   });
+ //
+ // })
