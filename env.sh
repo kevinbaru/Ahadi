50bd1cd457b296b5606128829ca03f4369c48116
git push -u origin <baru.kevin3@gmail.com>
2017-08-10 22:07:47 -0700

    rm

--- env.sh
+++
@@ -1,7 +0,0 @@
-
-export MONGODB_URI='mongodb://horizons:horizons@ds163232.mlab.com:63232/shchedulerslack';
-export SLACK_API_TOKEN='xoxb-214142836389-qGVjUwcLoxNeiRWp7r4JSW9q';
-export API_AI_TOKEN='5038698d30b4485394ecdfdf9bbea627';
-export GOOGLE_CLIENT_ID='569623010206-gh0149lad8hbms502t2t9hjbed54qclt.apps.googleusercontent.com';
-export GOOGLE_CLIENT_SECRET='qnafrV_JxfCPWKUkTnfoQ-QP';
-export DOMAIN='http://9d64224f.ngrok.io';
