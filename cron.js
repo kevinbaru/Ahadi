9e964d526d777ba3bde955cc6fd02c2d6957504d
kevinbaru <baru.kevin3@gmail.com>
2017-08-30 11:05:40 -0700

    Update cron.js

--- cron.js
+++ cron.js
@@ -5,9 +5,7 @@ console.log('Hello I am runnin!!')
 var{User, Reminder}= require('./models');
 var {web}= require('./slackBot');
 var moment= require('moment');
-console.log('now',moment(Date.now()).format('YYYY-MM-DD'))
 
-console.log('tomorrow',moment(Date.now()).add(1,'day').format())
 
 Reminder.find({date: moment(Date.now()).format('YYYY-MM-DD')}).populate('user')
 .then(function(reminders){
@@ -15,7 +13,7 @@ Reminder.find({date: moment(Date.now()).format('YYYY-MM-DD')}).populate('user')
   
   web.chat.postMessage(reminders[0].user.slackDMId,'TODAY\'S REMINDERS',function(){
     reminders.map((reminder,index)=>{
-      console.log(reminder.reminder.subject,'fgggg')
+  
       web.chat.postMessage(reminder.user.slackDMId,`Remember to ${reminder.reminder.subject}`,function(){
         if(index===reminders.length-1){
 
@@ -38,10 +36,4 @@ Reminder.find({date: moment(Date.now()).format('YYYY-MM-DD')}).populate('user')
   console.log('ReminderError',err)
 })
 
- // User.findOne()
- // .then(function(user){
- //   web.chat.postMessage(user.slackDMId,'Current time is '+ new Date(),function() {
- //     process.exit(0)
- //   });
- //
- // })
+
